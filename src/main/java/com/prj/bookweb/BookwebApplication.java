490b8a6436e1e2cfd8fb737cba9efeddede845dc
seongbae-ji <wltjdqo55@gmail.com>
2024-06-24 15:06:06 +0900

    first commit

---
+++ src/main/java/com/prj/bookweb/BookwebApplication.java
@@ -0,0 +1,13 @@
+package com.prj.bookweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prj/bookweb/BookwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj.bookweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
